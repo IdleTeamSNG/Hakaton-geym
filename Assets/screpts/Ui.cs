d43db4ae851f94476e2d8f7f3a7429f225e1050e
mandarunka <85961397+mandarunka@users.noreply.github.com>
2021-09-21 17:04:14 +0300

    srg

--- Assets/screpts/Ui.cs
+++ Assets/screpts/Ui.cs
@@ -1,13 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ui : MonoBehaviour
-{
+{ 
+    Core cr = new Core();
+    List<int> Case = new List<int>();
+   
+    public GameObject txt;
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   Case = cr.getNewCase(6, 1);
+
+        for (int i = 0; i < Case.Count - 1; i++)
+        {
+            Debug.Log(Case[i]);
+            if (i == 0)
+            {
+                txt.GetComponent<Text>().text = txt.GetComponent<Text>().text + System.Convert.ToString(Case[i]) + "x";
+            }
+            else if(i == (Case.Count - 1))
+            {
+                txt.GetComponent<Text>().text = txt.GetComponent<Text>().text + System.Convert.ToString(Case[i]) + "=";
+            }
+            else
+            {
+                txt.GetComponent<Text>().text = txt.GetComponent<Text>().text + System.Convert.ToString(Case[i]) + "x";
+            }
+        }
 
     }
 
