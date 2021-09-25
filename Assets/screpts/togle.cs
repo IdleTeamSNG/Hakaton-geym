8a80fe017b1498e90c6e343c6fb8bc8aa53e53b9
mandarunka <85961397+mandarunka@users.noreply.github.com>
2021-09-25 14:25:54 +0300

    Music

--- Assets/screpts/togle.cs
+++ Assets/screpts/togle.cs
@@ -10,8 +10,8 @@ public class togle : MonoBehaviour
     [Tooltip("Togle value.")]
     public bool value;
 
-    private AudioSource audioSr;
-    public GameObject audioSrObj;
+    public AudioSource audioSr;
+    
     public AudioClip ClicAu;
 
     private static readonly int Value = Animator.StringToHash(name: "Value");
@@ -19,10 +19,7 @@ public class togle : MonoBehaviour
     public event Action<bool> OnToggle;
 
 
-    public void Start()
-    {
-        audioSr = audioSrObj.GetComponent<AudioSource>();
-    }
+    
     private void Awake()
     {
         
