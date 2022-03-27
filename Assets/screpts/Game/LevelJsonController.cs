cea55c849c96fb1c81f3419aca656e8014fc9236
mandarunka <85961397+mandarunka@users.noreply.github.com>
2022-03-27 12:29:20 +0300

    EventJsonController

--- Assets/screpts/Game/LevelJsonController.cs
+++ Assets/screpts/Game/LevelJsonController.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System;
 
 public class LevelJsonController : MonoBehaviour
 {
     public int LevelNumb;
     public LevelScenario LevelScenarioCl;
+    public int iter = 0;
+    public string[] Instuction;
+    public delegate void Insr(string Argument);
+    public static event Insr Qestion;
+    public static event Insr Image;
+    public static event Insr Messege;
 
     void Start()
     {
@@ -21,5 +28,27 @@ public class LevelJsonController : MonoBehaviour
 
 
 
+    }
+    public void next()
+    {
+        
+        Instuction = LevelScenarioCl.Scenario[0].Split('/');
+       switch(Instuction[0] )
+        {
+            case "1":
+                Qestion(Instuction[1]);
+                break;
+
+            case "2":
+                Messege(Instuction[1]);
+                break;
+
+            case "3":
+                Image(Instuction[1]);
+                break;
+
+        }
+
+        
     }
 }
